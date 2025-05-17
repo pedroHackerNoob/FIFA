7b153a95a276a2de3231368eea408a230fbd672c
Tyler Brock <pedrohardbass@gmail.com>
2025-05-17 14:37:19 -0700

    demo v1 banner added

--- models/Player.py
+++ models/Player.py
@@ -6,7 +6,7 @@ def info_players( engine , database, text):
         # print in row
         result= conn.execute(text("SELECT * FROM jugador"))
         print("-----------------------------------------------------------------------------------------------------------------------------")
-        print("|Informacion de jugadores:")
+        print("|All players info")
         print("-----------------------------------------------------------------------------------------------------------------------------")
         for row in result:
             print(f'|ID: {row[0]} | name: {row[1]} | country: {row[2]} | job: {row[3]} | position {row[4]} | top: {row[5]} | level: {row[6]} | txt: {row[7]} | TeamId:{row[8]}')
