8f4ee9778f15cb479281b0374abb06586c5de89b
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2025-05-21 22:59:26 -0700

    show matches cleaned code

--- models/Match.py
+++ models/Match.py
@@ -21,7 +21,7 @@ from rich.console import Console
 from rich.table import Table
 console_r = Console()
 # READ
-def show_match(select, Session, engine, txt):
+def show_match(Session):
     try:
         ui_text.charge_bar()
         table = Table(title="All matches", show_header=True, header_style="bold magenta", show_lines=True)
@@ -46,38 +46,14 @@ def show_match(select, Session, engine, txt):
     except Exception as e:
         print(f"Error displaying matches: {str(e)}")
 # UPDATE
-def update_match(engine, text):
-    show_match(engine, text)
-    with engine.connect() as conn:
-        id_partido = int(input("| Enter id_match: "))
-        fecha = datetime
-        lugar = input("| Enter place: ")
-        id_equipo1 = int(input("| Enter id_team visitors: "))
-        id_equipo2 = int(input("| Enter id_team locals: "))
+def update_match(Session):
+    show_match(Session)
+    id_partido = int(input("| Enter id_match: "))
+    fecha = datetime
+    lugar = input("| Enter place: ")
+    id_equipo1 = int(input("| Enter id_team visitors: "))
+    id_equipo2 = int(input("| Enter id_team locals: "))
 
-        # prompt
-        conn.execute(
-            text("""
-                 UPDATE partido
-                 SET fecha   = :fecha,
-                     lugar     = :lugar,
-                     idEquipo1 = :idEquipo1,
-                     idEquipo2 = :idEquipo2
-                     
-                 WHERE idpartido = :idpartido
-                 """),
-            [{
-                "fecha": fecha,
-                "lugar": lugar,
-                "idEquipo1": id_equipo1,
-                "idEquipo2": id_equipo2,
-                "idpartido": id_partido
-            }]
-        )
-
-        # ejecutar cambios
-        conn.commit()
-    show_match(engine, text)
 # CREATE
 def create_match(engine, text):
     show_match(engine, text)
